990c3b32726c7769e14e4b8e794ba2fc9e3c719c
Douglas Oliveira da Silva <douglasoliveira.dasilva@hotmail.com>
2025-05-20 12:07:10 -0300

    feat: adiciona a classe DTO para melhor organização da API , pensando em futuras manutenções e manter a classe Model segura.

---
+++ src/main/java/devDouglas/java/CadastroCarros/dto/CarroDTO.java
@@ -0,0 +1,19 @@
+package devDouglas.java.CadastroCarros.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CarroDTO {
+
+    private Long id;
+    private String modelo;
+    private String marca;
+    private int ano;
+    private String cor;
+    private String placa;
+
+}
