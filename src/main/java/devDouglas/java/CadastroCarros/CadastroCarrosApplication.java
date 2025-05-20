526b4893869a6775a1454c03a6059fd9e6b50b13
Douglas-Oliveira-silva <douglasoliveira.dasilva@hotmail.com>
2025-05-20 05:18:51 -0300

    docs: Primeiro commit do projeto.

---
+++ src/main/java/devDouglas/java/CadastroCarros/CadastroCarrosApplication.java
@@ -0,0 +1,13 @@
+package devDouglas.java.CadastroCarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroCarrosApplication.class, args);
+	}
+
+}
