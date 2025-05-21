f62abde88c344384cef46407cf2be08545a5672c
Douglas-Oliveira-silva <douglasoliveira.dasilva@hotmail.com>
2025-05-21 05:31:05 -0300

    feat: adiciona anotations NotBlank e Pattern para exibição de mensagem e obrigatoriedade no padrão da placa do carro por ex

--- src/main/java/devDouglas/java/CadastroCarros/model/CarroModel.java
+++ src/main/java/devDouglas/java/CadastroCarros/model/CarroModel.java
@@ -2,6 +2,9 @@ package devDouglas.java.CadastroCarros.model;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,23 +20,28 @@ import lombok.ToString;
 public class CarroModel {
 
    @Id
-   @GeneratedValue
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
    @Column(name="id")
    private Long id;
 
    @Column(name="modelo")
+   @NotBlank(message = " O Modelo é obrigatório")
    private String modelo;
 
    @Column(name="marca")
+   @NotBlank(message = "A Marca é obrigatória")
    private String marca;
 
    @Column(name="ano")
+   @Min(value = 1886, message = "Ano inválido") // 1886 = ano do primeiro carro)
    private int ano;
 
    @Column(name="cor")
+   @NotBlank(message = "A cor é obrigatória")
    private String cor;
 
    @Column(unique = true)
+   @Pattern(regexp = "[A-Z]{3}-?\\d{4}", message = "Placa no formato inválido")
    private String placa;
 
 }
